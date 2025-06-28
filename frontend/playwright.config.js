d449250c4eddd01abbb5943d0ba799088560947f
SandActor <luka.timohin@yandex.ru>
2025-06-28 12:22:46 +0500

    1 проверка

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   webServer: {
-    command: 'npx start-server -s ./frontend',
+    command: 'npx start-server -s ./frontend -p 5002',
     url: 'http://localhost:5002',
     timeout: 60000,
     reuseExistingServer: true,
